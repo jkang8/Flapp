84b3c776cde4e47b41d1fed3a4d26a9d0be7e007
John Kang <johnkang.h@gmail.com>
2015-05-25 02:40:55 -0700

    Created async task that connects to Flickr
    * Created loadImages() which is subclass of AsyncTask, in order to
      connect to Flickr

--- app/src/main/java/com/example/john/flapp/MainActivity.java
+++ app/src/main/java/com/example/john/flapp/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.john.flapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class MainActivity extends ActionBarActivity {
+    //private static final String API_KEY = "e191b08c93cdc17ab14f6c6937cdfb10";
+    //private static final String API_SECRET = "5188aa164ef85c88";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new loadImages().execute();
     }
 
     @Override
@@ -34,5 +47,42 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class loadImages extends AsyncTask<String, Integer, JSONObject> {
+        private ProgressDialog progressDialog;
+        private Integer PER_PAGE = 10;
+        private static final String API_KEY = "e191b08c93cdc17ab14f6c6937cdfb10";
+        //URL endpoint = new URL("https://api.flickr.com/services/feeds/photos_public.gne?format=json");
+        //URL url = new URL("https://www.flickr.com/services/api/explore/flickr.photos.getRecent");
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+            HttpURLConnection connection;
+            int status;
+            try {
+                URL url = new URL("https://www.flickr.com/services/api/explore/flickr.photos.getRecent&api_key="
+                                    + API_KEY
+                                    +"&per_page=" + PER_PAGE
+                                    + "&format=json"
+                                    );
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                status = connection.getResponseCode();
+                Log.d("connection", "status " + status);
+            } catch(MalformedURLException e) {
+                System.err.println("Bad URL: " + e);
+            } catch(IOException e) {
+                System.err.println("IO Exception " + e);
+            }
+        return null;
+        }
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setMessage("Loading images from Flickr");
+        }
+
     }
 }
