62ec3d848d48c5e1c498d72f1729e7729e812ccc
John Kang <johnkang.h@gmail.com>
2015-05-25 20:55:32 -0700

    Created FlickrPublic class to hold public feed

--- app/src/main/java/com/example/john/flapp/MainActivity.java
+++ app/src/main/java/com/example/john/flapp/MainActivity.java
@@ -8,12 +8,17 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +29,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new loadImages().execute();
+        new loadPublicImages().execute();
     }
 
     @Override
@@ -49,7 +54,7 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private class loadImages extends AsyncTask<String, Integer, JSONObject> {
+    private class loadPublicImages extends AsyncTask<String, Integer, JSONObject> {
         private ProgressDialog progressDialog;
         private Integer PER_PAGE = 10;
         private static final String API_KEY = "e191b08c93cdc17ab14f6c6937cdfb10";
@@ -61,15 +66,54 @@ public class MainActivity extends ActionBarActivity {
             HttpURLConnection connection;
             int status;
             try {
-                URL url = new URL("https://www.flickr.com/services/api/explore/flickr.photos.getRecent&api_key="
+                /*URL url = new URL("https://www.flickr.com/services/api/explore/flickr.photos.getRecent&api_key="
                                     + API_KEY
                                     +"&per_page=" + PER_PAGE
                                     + "&format=json"
-                                    );
+                                    );*/
+                // Connect to public photos endpoint
+                URL url = new URL("https://api.flickr.com/services/feeds/photos_public.gne?format=json");
                 connection = (HttpURLConnection) url.openConnection();
                 connection.connect();
                 status = connection.getResponseCode();
                 Log.d("connection", "status " + status);
+
+                // Read in endpoint response
+                InputStream in = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                int counter = 1, START_LINE = 1;
+                while((line = reader.readLine()) != null ) {
+                    if(counter > START_LINE) {
+                        sb = sb.append(line);
+                        Log.d("connection", "line: " + line);
+                    }
+                    counter++;
+                }
+
+                String publicPhotoData = "{" + sb.toString() + "}";
+                try {
+                    JSONObject jsonPhotoData = new JSONObject(publicPhotoData);
+                    FlickrPublic flickrPublic = new FlickrPublic(jsonPhotoData);
+                    ArrayList<FlickrPublic> photoArray = flickrPublic.FlickrPhotos(publicPhotoData);
+                    for(int i=0; i< photoArray.size(); ++i) {
+                        Log.d("connection", "array: " + photoArray.get(i));
+                    }
+                } catch (JSONException e) {
+                    Log.d("connection", "JSONException main: " + e);
+                }
+                /*
+                try {
+                    JSONObject jsonPublic = new JSONObject("{ " +publicPhotoData+ "}");
+                    JSONArray photoArray = jsonPublic.optJSONArray("items");
+                    for(int i = 0; i < photoArray.length(); i++) {
+                        JSONObject photo = (JSONObject) photoArray.get(i);
+                        Log.d("connection", photo.optString("title"));
+                    }
+                } catch (JSONException e) {
+                    Log.d("connection", "JSONException: " + e);
+                } */
             } catch(MalformedURLException e) {
                 System.err.println("Bad URL: " + e);
             } catch(IOException e) {
